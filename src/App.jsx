e1b9137dcad03d89152bbf41f85455cccb3e686b
devfromnyc <devfromnyc@gmail.com>
2023-08-29 12:55:22 -0400

    feat: create todo item component and implement session storage for todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import { NewTodoForm } from "./NewTodoForm";
+import { TodoList } from "./TodoList";
 import "./styles.css";
 
 export default function App () {
   
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS");
+    if(localValue == null) return [];
+    return JSON.parse(localValue);
+  });
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos));
+  }, [todos])
 
   function addTodo(title) {
     setTodos(currentTodos => {
@@ -36,20 +45,7 @@ export default function App () {
     <React.Fragment>
       <NewTodoForm onSubmit={addTodo} />
       <h1 className="header">Todo List</h1>
-      <ul className="list">
-        {todos.length === 0 && "No Todos"}
-        {todos.map(todo =>{
-          return(
-            <li key={todo.id}>
-              <label>
-                <input type="checkbox" checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)} />
-                {todo.title}
-              </label>
-              <button className="btn btn-danger" onClick={() => deleteTodo(todo.id)}>Delete</button>
-            </li>
-          )
-        })}
-      </ul>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
     </React.Fragment>
   )
 }
