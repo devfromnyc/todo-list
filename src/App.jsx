2e120fa0c12f0018d3e5369bc7f0fd3a1423df26
devfromnyc <devfromnyc@gmail.com>
2023-08-29 13:21:01 -0400

    install: prettier vs code

--- src/App.jsx
+++ src/App.jsx
@@ -3,42 +3,41 @@ import { NewTodoForm } from "./NewTodoForm";
 import { TodoList } from "./TodoList";
 import "./styles.css";
 
-export default function App () {
-  
+export default function App() {
   const [todos, setTodos] = useState(() => {
     const localValue = localStorage.getItem("ITEMS");
-    if(localValue == null) return [];
+    if (localValue == null) return [];
     return JSON.parse(localValue);
   });
 
   useEffect(() => {
     localStorage.setItem("ITEMS", JSON.stringify(todos));
-  }, [todos])
+  }, [todos]);
 
   function addTodo(title) {
-    setTodos(currentTodos => {
+    setTodos((currentTodos) => {
       return [
         ...currentTodos,
-        { id: crypto.randomUUID(), title, completed: false }
-      ]
-    })
+        { id: crypto.randomUUID(), title, completed: false },
+      ];
+    });
   }
 
-  function toggleTodo (id, completed){
-    setTodos(currentTodos => {
-      return currentTodos.map(todo =>{
-        if (todo.id === id){
-          return { ...todo, completed }
+  function toggleTodo(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed };
         }
-        return todo
-      })
-    })
+        return todo;
+      });
+    });
   }
 
-  function deleteTodo(id){
-    setTodos(currentTodos => {
-      return currentTodos.filter(todo => todo.id !== id);
-    })
+  function deleteTodo(id) {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
   }
 
   return (
@@ -47,5 +46,5 @@ export default function App () {
       <h1 className="header">Todo List</h1>
       <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
     </React.Fragment>
-  )
+  );
 }
